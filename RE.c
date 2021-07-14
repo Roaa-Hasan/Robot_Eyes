315bf1f17414a982f9c2d286e32af36737c40ef6
Roaa Hasan <85456781+Roaa-Hasan@users.noreply.github.com>
2021-07-14 15:26:42 +0300

    Add files via upload

---
+++ RE.c
@@ -0,0 +1,15 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(4, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(5, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
